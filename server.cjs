fed3c93fe71a0e42bdf98117b5094371791c9e14
Hunter Jorgensen <ajorgense1@chewy.com>
2024-04-10 20:32:06 -0700

    Adding mongoose/cookie code

--- server.cjs
+++ server.cjs
@@ -1,9 +1,22 @@
 const express = require('express');
 const pokemon = require('./backend/pokemon.api.cjs');
 const users = require('./backend/user.api.cjs')
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
+
+const mongoDBEndpoint = 'mongodb+srv://hunter:_____@seawebdevfall2021.ykjok.mongodb.net/?retryWrites=true&w=majority&appName=SeaWebDevFall2021'
+mongoose.connect(mongoDBEndpoint, {
+    useNewUrlParser: true,
+})
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,6 +34,8 @@ app.get('/', function(request, response) {
 app.put('/', function(request, response) {
     response.send("This is a PUT request")
 })
+
+
 
 app.listen(8000, function() {
     console.log("Starting app now...")
